5b6f5b09357fd940df0629d8bad4a9b0c484b77f
Score2 <836696023@qq.com>
2021-09-08 22:27:44 +0800

    [2.0-INITIAL] Added CommandHandler.

---
+++ src/main/kotlin/fastscript/command/CommandHandler.kt
@@ -0,0 +1,13 @@
+package fastscript.command
+
+import taboolib.common.LifeCycle
+import taboolib.common.platform.SkipTo
+import taboolib.common.platform.command.CommandHeader
+
+@SkipTo(LifeCycle.LOAD)
+@CommandHeader(name = "FastScript", aliases = ["fs", "fscript"])
+object CommandHandler {
+
+
+
+}
